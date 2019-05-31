4417c43e4cd99da951b8ba42283b52c06aa525cb
MauricioFIUBA <31808401+MauricioFIUBA@users.noreply.github.com>
2019-05-30 23:38:51 -0300

    Create HolaMundoTest.java

---
+++ test/fiuba/algo3/HolaMundoTest.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.ejemplo1;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class FooTest {
+
+	@Test
+	public void returnNone(){
+        HolaMundo hola = new HolaMundo();
+        Assert.assertEquals(None, result);
+	}
+}
