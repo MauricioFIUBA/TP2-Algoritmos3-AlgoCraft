8e01c4b336e6fbaae0f3f83980107878be7e8dab
MauricioFIUBA <31808401+MauricioFIUBA@users.noreply.github.com>
2019-05-31 00:14:00 -0300

    Update HolaMundoTest.java

--- test/fiuba/algo3/ejemplo1/HolaMundoTest.java
+++ test/fiuba/algo3/ejemplo1/HolaMundoTest.java
@@ -8,7 +8,7 @@ public class HolaMundoTest {
 	@Test
 	public void returnNone(){
         HolaMundo hola = new HolaMundo();
-	String result = hola.("mundo!");
+	String result = hola.Hola("mundo!");
         Assert.assertEquals("Hola mundo!", result);
 	}
 }
