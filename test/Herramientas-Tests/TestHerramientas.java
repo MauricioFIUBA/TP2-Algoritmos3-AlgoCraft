be75a5c125832f0a41052b45b898323596a9fd61
Santi Piragine <santiago.piragine96@hotmail.com>
2019-06-05 15:35:24 -0300

    Pruebas actualizadas correspondientes al nuevo modelo de Herramienta.

--- test/Herramientas-Tests/TestHerramientas.java
+++ test/Herramientas-Tests/TestHerramientas.java
@@ -1,49 +1,50 @@
+import Materiales.*;
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 public class TestHerramientas {
     @Test
     public void test01SeInstanciaUnHachaDeMadera() {
-        Herramienta hachaDeMadera = new HachaDeMadera();
+        Herramienta hachaDeMadera = new Hacha(new Madera());
         assertNotNull(hachaDeMadera);
     }
 
     @Test
     public void test02SeCreaUnHachaDeMaderaConSuDurabilidadYFuerzaCorrespondiente() {
-        Herramienta hachaDeMadera = new HachaDeMadera();
+        Herramienta hachaDeMadera = new Hacha(new Madera());
         assertEquals(hachaDeMadera.getDurabilidad(), 100);
         assertEquals(hachaDeMadera.getFuerza(), 2);
     }
     @Test
     public void test03SeInstanciaUnPicoDeMadera(){
-        Herramienta picoDeMadera = new PicoDeMadera();
+        Herramienta picoDeMadera = new Pico(new Madera());
         assertNotNull(picoDeMadera);
     }
     @Test
     public void test04SeCreaUnPicoDeMaderaConSuDurabilidadYFuerzaCorrespondiente(){
-        Herramienta picoDeMadera = new PicoDeMadera();
+        Herramienta picoDeMadera = new Pico(new Madera());
         assertEquals(picoDeMadera.getDurabilidad(),100);
         assertEquals(picoDeMadera.getFuerza(),2);
     }
     @Test
     public void test05SeInstanciaUnHachaDeMetal(){
-        Herramienta hachaDeMetal = new HachaDeMetal();
+        Herramienta hachaDeMetal = new Hacha(new Metal());
         assertNotNull(hachaDeMetal);
     }
     @Test
     public void test06SeCreaUnHachaDeMetalConSuDurabilidadYFuerzaCorrespondiente(){
-        Herramienta hachaDeMetal = new HachaDeMetal();
+        Herramienta hachaDeMetal = new Hacha(new Metal());
         assertEquals(hachaDeMetal.getDurabilidad(),400);
         assertEquals(hachaDeMetal.getFuerza(),10);
     }
     @Test
     public void test07SeInstanciaUnPicoDeMetal(){
-        Herramienta picoDeMetal = new PicoDeMetal();
+        Herramienta picoDeMetal = new Pico(new Metal());
         assertNotNull(picoDeMetal);
     }
     @Test
     public void test08SeCreaUnPicoDeMetalConSuDurabilidadYFuerzaCorrespondiente(){
-        Herramienta picoDeMetal = new PicoDeMetal();
+        Herramienta picoDeMetal = new Pico(new Metal());
         assertEquals(picoDeMetal.getDurabilidad(),400);
         assertEquals(picoDeMetal.getFuerza(),12);
     }
