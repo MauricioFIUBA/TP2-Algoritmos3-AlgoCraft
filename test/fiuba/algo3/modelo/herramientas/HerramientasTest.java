712fb00e17592878e8bd8e72fbcd06e31902364f
Santi Piragine <santiago.piragine96@hotmail.com>
2019-06-13 01:04:13 -0300

    Se agregaron tests de Pico Fino con todos los materiales.

--- test/fiuba/algo3/modelo/herramientas/HerramientasTest.java
+++ test/fiuba/algo3/modelo/herramientas/HerramientasTest.java
@@ -2,8 +2,9 @@ package fiuba.algo3.modelo.herramientas;
 import fiuba.algo3.modelo.materiales.*;
 
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
+
 public class HerramientasTest {
     @Test
     public void test01SeInstanciaUnHachaDeMadera() {
@@ -18,18 +19,83 @@ public class HerramientasTest {
         assertEquals(hachaDeMadera.getFuerza(), 2);
     }
     @Test
-    public void test03SeInstanciaUnPicoDeMadera(){
+    public void test03SeCreaUnHachaDeMaderaYSeLaUsaContraUnBloqueDeMaderaYBajaLaDurabilidadDelHacha(){
+        Hacha hachaDeMadera = new Hacha(new Madera());
+        Madera madera = new Madera();
+        int durabilidadHacha = hachaDeMadera.getDurabilidad();
+        madera.esDesgastadoPor(hachaDeMadera);
+        assertEquals(hachaDeMadera.getDurabilidad(), durabilidadHacha- (hachaDeMadera.getFuerza()));
+    }
+    @Test
+    public void test04SeCreaUnHachaDeMaderaYSeLaUsaContraUnBloqueDePiedraYBajaLaDurabilidadDelHacha(){
+        Hacha hachaDeMadera = new Hacha(new Madera());
+        Piedra piedra = new Piedra();
+        int durabilidadHacha = hachaDeMadera.getDurabilidad();
+        piedra.esDesgastadoPor(hachaDeMadera);
+        assertEquals(hachaDeMadera.getDurabilidad(), durabilidadHacha- (hachaDeMadera.getFuerza()));
+    }
+    @Test
+    public void test05SeCreaUnHachaDeMaderaYSeLaUsaContraUnBloqueDeMetalYBajaLaDurabilidadDelHacha(){
+        Hacha hachaDeMadera = new Hacha(new Madera());
+        Metal metal = new Metal();
+        int durabilidadHacha = hachaDeMadera.getDurabilidad();
+        metal.esDesgastadoPor(hachaDeMadera);
+        assertEquals(hachaDeMadera.getDurabilidad(), durabilidadHacha- (hachaDeMadera.getFuerza()));
+    }
+    @Test
+    public void test06SeCreaUnHachaDeMaderaYSeLaUsaContraUnBloqueDeDiamanteYBajaLaDurabilidadDelHacha(){
+        Hacha hachaDeMadera = new Hacha(new Madera());
+        Diamante diamante = new Diamante();
+        int durabilidadHacha = hachaDeMadera.getDurabilidad();
+        diamante.esDesgastadoPor(hachaDeMadera);
+        assertEquals(hachaDeMadera.getDurabilidad(), durabilidadHacha- (hachaDeMadera.getFuerza()));
+    }
+
+    @Test
+    public void test07SeInstanciaUnPicoDeMadera(){
         Herramienta picoDeMadera = new Pico(new Madera());
         assertNotNull(picoDeMadera);
     }
     @Test
-    public void test04SeCreaUnPicoDeMaderaConSuDurabilidadYFuerzaCorrespondiente(){
+    public void test08SeCreaUnPicoDeMaderaConSuDurabilidadYFuerzaCorrespondiente(){
         Herramienta picoDeMadera = new Pico(new Madera());
         assertEquals(picoDeMadera.getDurabilidad(),100);
         assertEquals(picoDeMadera.getFuerza(),2);
     }
     @Test
-    public void test05SeInstanciaUnHachaDeMetal(){
+    public void test09SeCreaUnPicoDeMaderaYSeLoUsaContraUnBloqueDeMaderaYElPicoDeMaderaBajaSuDurabilidad(){
+        Pico picoDeMadera = new Pico(new Madera());
+        Madera madera = new Madera();
+        int durabilidadPicoDeMadera = picoDeMadera.getDurabilidad();
+        madera.esDesgastadoPor(picoDeMadera);
+        assertEquals(picoDeMadera.getDurabilidad(), durabilidadPicoDeMadera - (picoDeMadera.getFuerza()));
+    }
+    @Test
+    public void test10SeCreaUnPicoDeMaderaYSeLoUsaContraUnBloqueDePiedraYElPicoDeMaderaBajaSuDurabilidad(){
+        Pico picoDeMadera = new Pico(new Madera());
+        Piedra piedra = new Piedra();
+        int durabilidadPicoDeMadera = picoDeMadera.getDurabilidad();
+        piedra.esDesgastadoPor(picoDeMadera);
+        assertEquals(picoDeMadera.getDurabilidad(), durabilidadPicoDeMadera - (picoDeMadera.getFuerza()));
+    }
+    @Test
+    public void test11SeCreaUnPicoDeMaderaYSeLoUsaContraUnBloqueDeMetalYElPicoDeMaderaBajaSuDurabilidad(){
+        Pico picoDeMadera = new Pico(new Madera());
+        Metal metal = new Metal();
+        int durabilidadPicoDeMadera = picoDeMadera.getDurabilidad();
+        metal.esDesgastadoPor(picoDeMadera);
+        assertEquals(picoDeMadera.getDurabilidad(), durabilidadPicoDeMadera - (picoDeMadera.getFuerza()));
+    }
+    @Test
+    public void test12SeCreaUnPicoDeMaderaYSeLoUsaContraUnBloqueDeDiamanteYElPicoDeMaderaBajaSuDurabilidad(){
+        Pico picoDeMadera = new Pico(new Madera());
+        Diamante diamante = new Diamante();
+        int durabilidadPicoDeMadera = picoDeMadera.getDurabilidad();
+        diamante.esDesgastadoPor(picoDeMadera);
+        assertEquals(picoDeMadera.getDurabilidad(), durabilidadPicoDeMadera - (picoDeMadera.getFuerza()));
+    }
+    @Test
+    public void test09SeInstanciaUnHachaDeMetal(){
         Herramienta hachaDeMetal = new Hacha(new Metal());
         assertNotNull(hachaDeMetal);
     }
@@ -61,4 +127,38 @@ public class HerramientasTest {
         assertEquals(picoFino.getDurabilidad(),1000);
         assertEquals(picoFino.getFuerza(),20);
     }
+    @Test
+    public void test11SeCreaUnPicoFinoYSeLoUsaContraUnBloqueDeMaderaYElPicoFinoNoSeDesgasta(){
+        PicoFino picoFino = new PicoFino();
+        Madera madera = new Madera();
+        int esperado = picoFino.getDurabilidad();
+        madera.esDesgastadoPor(picoFino);
+        assertEquals(picoFino.getDurabilidad(),esperado);
+    }
+    @Test
+    public void test12SeCreaUnPicoFinoYSeLoUsaContraUnBloqueDePiedraYElPicoFinoNoSeDesgasta(){
+        PicoFino picoFino = new PicoFino();
+        Piedra piedra = new Piedra();
+        int esperado = picoFino.getDurabilidad();
+        piedra.esDesgastadoPor(picoFino);
+        assertEquals(picoFino.getDurabilidad(), esperado);
+    }
+    @Test
+    public void test13SeCreaUnPicoFinoYSeLoUsaContraUnBloqueDeMetalYElPicoFinoNoSeDesgasta(){
+        PicoFino picoFino = new PicoFino();
+        Metal metal = new Metal();
+        int esperado = picoFino.getDurabilidad();
+        metal.esDesgastadoPor(picoFino);
+        assertEquals(picoFino.getDurabilidad(), esperado);
+    }
+    @Test
+    public void test14SeCreaUnPicoFinoYSeLoUsaContraUnBloqueDeDiamanteYElPicoFinoSeDesgasta(){
+        PicoFino picoFino = new PicoFino();
+        Diamante diamante = new Diamante();
+        //Tuve que castear a int para que corra el test.
+        int esperado = (int) (picoFino.getDurabilidad() - (picoFino.getDurabilidad()*0.1));
+        diamante.esDesgastadoPor(picoFino);
+        assertEquals(picoFino.getDurabilidad(),esperado);
+    }
+
 }
