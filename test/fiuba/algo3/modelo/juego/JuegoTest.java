84e85d961f40b32ce24d7ddcd23043838effbb10
Santi Piragine <santiago.piragine96@hotmail.com>
2019-06-19 20:36:27 -0300

    Se agrega el test del juego, el cual simula y verifica que se inicie correctamente el mapa con un jugador y con los materiales correspondientes.

--- test/fiuba/algo3/modelo/juego/JuegoTest.java
+++ test/fiuba/algo3/modelo/juego/JuegoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JuegoTest {
+
     @Test
     public void test01SeInicializaElJuegoConUnTableroDeXPosicionesUnJugadorYConLosElementosDistribuidosEnElMapa() {
         Juego juego = new Juego();
@@ -13,6 +14,7 @@ public class JuegoTest {
         /*Capacidad del mapa(50*50) - maderas(16) - metales(4) - piedras(4) - diamantes(4)*/
         Integer esperado = (20*20)-29;
         Integer elementosDelJuego = mapaDelJuego.obtenerCapacidadDelMapa();
+
         assertEquals(elementosDelJuego,esperado);
     }
 }
